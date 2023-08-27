9734c1b335811384d8f45c0c4b1d8bd65ddec6ee
Misk Abdalla <miskmabdalla@gmail.com>
2023-08-27 13:52:13 +0300

    add message logic

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ app.use(express.json());
 app.post('/webhook', (req, res) => {
     console.log(JSON.stringify(req.body, null, 2));
     res.status(200).send('OK');
+    const incomingMessage = req.body.messages[0][0].text.body;
+    if (incomingMessage == "hi")
+        console.log("hi recieved");
+    else if (incomingMessage == "bye")
+        console.log("bye recieved");
+    else
+        console.log("other message recieved");
 });
 
 app.listen(port, () => {
