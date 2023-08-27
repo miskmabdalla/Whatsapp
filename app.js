e60f2481068f66f2fbda913a014322394a274b20
Misk Abdalla <miskmabdalla@gmail.com>
2023-08-27 13:02:41 +0300

    first init

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.post('/webhook', (req, res) => {
+  console.log('Received webhook data:', req.body);
+  res.status(200).send('OK');
+});
+
+app.listen(port, () => {
+  console.log(`Webhook server running at http://localhost:${port}`);
+});
