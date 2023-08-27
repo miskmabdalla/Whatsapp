7941017f18f6dc098c283b3b17b95d85cd480931
Misk Abdalla <miskmabdalla@gmail.com>
2023-08-27 14:52:20 +0300

    fix errors

--- sendMessage.js
+++ sendMessage.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const sendMessage = async (recipient, message) => {
   const url = 'https://wbapi.autochat.io/40dea042-d034-40ae-9794-b1c87d546d12/v1/messages';
   const headers = {
-    'Authorization': 'HFu3q2NLIF-4wBHhXDvEGw',
+    'Authorization': 'Bearer HFu3q2NLIF-4wBHhXDvEGw',
     'Content-Type': 'application/json'
   };
   const data = {
