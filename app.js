ca45bcbe5a59c96503e3527f7f9f4a95fa830568
Misk Abdalla <miskmabdalla@gmail.com>
2023-08-27 13:37:56 +0300

    used json parser

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-
+app.use(bodyParser.json());
 app.post('/webhook', (req, res) => {
   console.log('Received webhook data:', req.body);
   res.status(200).send('OK');
@@ -12,3 +12,4 @@ app.post('/webhook', (req, res) => {
 app.listen(port, () => {
   console.log(`Webhook server running at http://localhost:${port}`);
 });
+
