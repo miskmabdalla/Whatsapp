eca982f5f9a874fa83e6b1a402c6660b96d7fac0
Misk Abdalla <miskmabdalla@gmail.com>
2023-08-28 16:55:42 +0300

    add product verification

--- messageHandler.js
+++ messageHandler.js
@@ -1,13 +1,65 @@
-const handleIncomingMessage = (incomingMessage) => {
-    let responseMessage;
-  
-    if (incomingMessage.toLowerCase() === 'hello') {
-      responseMessage = 'Hi';
-    } else if (incomingMessage.toLowerCase() === 'goodbye') {
-      responseMessage = 'Bye';
-    } else {
-      responseMessage = 'I don\'t understand';
+const { use } = require("express/lib/application");
+
+//use db in the future to handle user states
+const userStates = {};
+
+const handleIncomingMessage = async (userId, incomingMessage) => {
+    // Initialize user state if not exists
+    if (!userStates[userId]) {
+        userStates[userId] = 'INIT';
     }
+
+    let responseMessage;
+    if (incomingMessage === 'back'){
+        userStates[userId] = 'INIT';
+    }
+    switch (userStates[userId]) {
+        case 'INIT':
+            responseMessage = 'Hi, how can I assist you today? You can verify your product or check your points' ; //could also use "type 1 for service 1, type 2 for service 2, etc."
+            userStates[userId] = 'ASKED_SERVICE';
+            break;
+        case 'ASKED_SERVICE':
+            if (incomingMessage.contains('verify')) {
+                responseMessage = 'You chose to verify your product, please enter your product ID';
+                userStates[userId] = 'VERIFY_PRODUCT';
+            }
+            else if (incomingMessage.contains('points')) {
+                responseMessage = 'You chose to check your points, please enter your ID'; //could also use their userID
+                userStates[userId] = 'CHECK_POINTS';
+            } 
+            break;
+        case 'VERIFY_PRODUCT':
+            if (incomingMessage === 'valid_number') {
+                responseMessage = 'Your product is valid';
+            }
+            else {
+                responseMessage = 'Your product is not valid';
+            }
+            userStates[userId] = 'INIT';
+            break;
+        case 'CHECK_POINTS':
+            if (incomingMessage === 'valid_id') {
+                responseMessage = 'You have 100 points';
+            }
+            else {
+                responseMessage = 'Invalid ID, type \'back\' to return to the start';
+            }
+            userStates[userId] = 'INIT';
+            break;
+        default:
+          responseMessage = 'I don\'t understand, type \'back\' to return to the start';
+      }
+
+
+
+
+    // if (incomingMessage.toLowerCase() === 'hello') {
+    //   responseMessage = 'Hi';
+    // } else if (incomingMessage.toLowerCase() === 'goodbye') {
+    //   responseMessage = 'Bye';
+    // } else {
+    //   responseMessage = 'I don\'t understand';
+    // }
   
      return responseMessage;
   };
--- webhookController.js
+++ webhookController.js
@@ -1,12 +1,13 @@
 const handleIncomingMessage = require('./messageHandler');
 const sendMessage = require('./sendMessage');
 
+
 // Single Responsibility: Handle webhook POST requests
 const webhookController = async (req, res) => {
   console.log(JSON.stringify(req.body, null, 2));
-
   const incomingMessage = req.body.messages[0][0].text.body;
-  const responseMessage =  await handleIncomingMessage(incomingMessage);
+  const userId = req.body.messages[0][0].from;
+  const responseMessage =  await handleIncomingMessage(userId, incomingMessage);
   await sendMessage('ejO4wSSx2ugfKGfOjRkgfg', responseMessage);
   console.log(responseMessage);  
 };
